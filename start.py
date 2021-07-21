910444035a358c547c8d24f501fa28c1995363b0
AT WORLD'S END <87578989+AT-WORLDS-END@users.noreply.github.com>
2021-07-21 09:17:52 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,6 @@
+from os import system 
+system ("git clone https://github.com/Rahulchoudhary17/Yamlok-UB && cd Yamlok-UB && python3 -m Yamlok")
+# try af
+print("𝙾𝙷 𝚂𝙺𝙴𝙼 𝚂𝚈𝚂𝚃𝙴𝙼 𝙶𝙾𝚃 𝙲𝚁𝙰𝚂𝙷𝙴𝙳")
+system("python3 -m Yamlok")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @YamlokSupport")
